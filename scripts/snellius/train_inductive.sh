8ab2738ee1b27dbaf2f18aff0e91a64fcec1391a
klim <klimzaporojets@gmail.com>
2025-08-12 22:46:48 +0200

    wip

---
+++ scripts/snellius/train_inductive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=tgi
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --time=24:00:00
+#SBATCH --partition=gpu_h100
+#SBATCH --gpus=1
+#SBATCH --output=logs/log_train_simkgc_transductive_%A.out
+
+
+source /home/kzaporojets/.bashrc
+#cd /home/kzaporojets/repositories/wikidata-temp/wikipedia-temp
+cd /home/kzaporojets/repositories/SimKGC
+#mkdir logs
+git pull
+
+
+OUTPUT_DIR=./checkpoint/wiki5m_ind/ bash scripts/train_wiki.sh wiki5m_ind
